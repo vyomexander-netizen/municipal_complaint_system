8edf09a26282f79836eff31d2fd2cc9030ea0fef
Satwik Nayak <vyomexander@gmail.com>
2026-06-19 22:00:59 +0530

    Load models from Hugging Face

--- model/classify_complaint.py
+++ model/classify_complaint.py
@@ -1,1 +1,18 @@
+import os
+from transformers import pipeline
 
+MODEL_ID = os.getenv("DEPARTMENT_MODEL_ID")
+
+classifier = pipeline(
+    "text-classification",
+    model=MODEL_ID,
+    tokenizer=MODEL_ID
+)
+
+def classify_complaint(text):
+    result = classifier(
+        text,
+        truncation=True,
+        max_length=128
+    )
+    return result[0]["label"]
